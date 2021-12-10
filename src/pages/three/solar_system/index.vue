912e6306fc6d95ffa3807d8f1a33ddffec2143da
王刘奇 <guochunxia@tal.com>
2021-12-10 21:39:01 +0800

    feat: 添加精灵标签，添加月球，

--- src/pages/three/solar_system/index.vue
+++ src/pages/three/solar_system/index.vue
@@ -9,33 +9,33 @@ import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-let scene, camera, renderer
+let scene, camera, renderer, moonGroup
 const s = 1000 // 三维场景显示范围控制系数，系数越大，显示的范围越大
 const width = window.innerWidth // 窗口宽度
 const height = window.innerHeight // 窗口高度
 const k = width / height // 窗口宽高比
 let sun // 太阳对象
 let earth // 地球
+let moon // 月球
 
 const controls = {
   太阳旋转速度: 0.02,
   地球旋转速度: 0.02,
   地球公转速度: 0.02,
+  月球公转速度: 0.02,
+  月球旋转速度: 0.02,
 }
 const gui = new GUI()
 gui.add(controls, '太阳旋转速度', 0, 0.2)
 gui.add(controls, '地球旋转速度', 0, 0.2)
 gui.add(controls, '地球公转速度', 0, 0.2)
+gui.add(controls, '月球公转速度', 0, 0.2)
+gui.add(controls, '月球旋转速度', 0, 0.2)
 export default defineComponent(
   {
     name: 'Index',
     mounted() {
       this.init()
-      const v1 = new THREE.Vector3(0, 1, 0)
-      const v2 = new THREE.Vector3(Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0)
-      const v3 = new THREE.Vector3(1, 1, 0).normalize()
-      console.log(v1.angleTo(v3))
-      console.log(v3)
     },
     methods: {
       init() {
@@ -66,6 +66,31 @@ export default defineComponent(
         sun = new THREE.Mesh(sunGeometry, sunMaterial)
         scene.add(sun)
         /**
+         * 创建标签
+         */
+        const canvas = document.createElement('canvas')
+        canvas.width = 512
+        canvas.height = 128
+        const c = canvas.getContext('2d')
+        // 矩形区域填充背景
+        c.fillStyle = 'transparent'
+        c.fillRect(0, 0, 512, 128)
+        c.beginPath()
+        c.translate(256, 64)
+        c.fillStyle = '#ffffff'
+        c.font = 'bold 48px 宋体'
+        c.textBaseline = 'middle'
+        c.textAlign = 'center'
+        c.fillText('太阳', 0, 0)
+        const tagTexture = new THREE.CanvasTexture(canvas)
+        const tagMaterial = new THREE.SpriteMaterial({
+          map: tagTexture,
+        })
+        const sprite = new THREE.Sprite(tagMaterial)
+        sprite.position.y = 120
+        sprite.scale.set(200, 80, 0)
+        scene.add(sprite)
+        /**
          * 地球
          * @type {SphereGeometry}
          */
@@ -85,6 +110,23 @@ export default defineComponent(
         earth.position.x = 200
         earth.rotateZ(-(Math.PI / 8))
         scene.add(earth)
+
+        /**
+         * 月球
+         * @type {SphereGeometry}
+         */
+        const moonGeometry = new THREE.SphereGeometry(10, 32, 32)
+        const moonTexture = loader.load('/stars/moon/moon.png')
+        const moonMaterial = new THREE.MeshLambertMaterial({
+          map: moonTexture,
+        })
+        moon = new THREE.Mesh(moonGeometry, moonMaterial)
+        moon.position.x = 50
+        moonGroup = new THREE.Group()
+        moonGroup.position.x = 300
+        moonGroup.add(moon)
+        scene.add(moonGroup)
+
         /**
          * 地球轨道线
          * @type {EllipseCurve}
@@ -92,7 +134,6 @@ export default defineComponent(
         const curve = new THREE.EllipseCurve(
           0, 0, 200, 200, // xRadius, yRadius
         )
-
         const curvePoints = curve.getPoints(50)
         const curveGeometry = new THREE.BufferGeometry().setFromPoints(curvePoints)
         const curveMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF })
@@ -137,6 +178,8 @@ export default defineComponent(
         q.setFromAxisAngle(new THREE.Vector3(Math.sin(Math.PI / 8), Math.cos(Math.PI / 8), 0), controls.地球旋转速度)
         // q.setFromAxisAngle(new THREE.Vector3(1, 1, 0).normalize(), controls.地球旋转速度)
         earth.quaternion.premultiply(q)
+        moon.rotateY(controls.月球旋转速度)
+        moonGroup.rotateY(controls.月球公转速度)
       },
       // 地球公转位置计算
       earthFllowSun() {
@@ -145,6 +188,8 @@ export default defineComponent(
         const revolution = controls.地球公转速度
         earth.position.x = Math.cos(revolution) * x0 + Math.sin(revolution) * z0
         earth.position.z = Math.cos(revolution) * z0 - Math.sin(revolution) * x0
+        moonGroup.position.x = earth.position.x
+        moonGroup.position.z = earth.position.z
       },
     },
   },
