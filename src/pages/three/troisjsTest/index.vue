c2820242980821604d4fa2f1dfaa8d71fda84594
王刘奇 <guochunxia@tal.com>
2022-01-14 15:23:48 +0800

    feat: 体验 troisJS。更新 ThreeJS 版本

---
+++ src/pages/three/troisjsTest/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Renderer :width="width" :height="height">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box>
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
+</template>
+
+<script>
+import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from 'troisjs'
+export default {
+  components: { Box, Camera, LambertMaterial, PointLight, Renderer, Scene },
+  data() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+  },
+}
+</script>
